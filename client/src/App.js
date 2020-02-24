b7184abb46ab713a0b0d9ea658304b9ebecc66d7
tlewandowski18 <tlewandowski18@gmail.com>
2020-02-23 20:22:10 -0500

    create class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+      super()
+      this.state = {
+          data: {}
+      }
+  }
+
+  componentDidMount(){
+      fetch(`http://localhost:5000/api/players`)
+          .then(res => res.json())
+          .then(res => {
+              console.log(res)
+          })
+          .catch(err => {
+              console.log(err)
+          })
+  }
+
+  render() {
+      return (
+          <div>Hello World</div>
+      )
+  }
 }
 
 export default App;
